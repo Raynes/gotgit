1bcfab15508a07b9c0b64394ad152a174d7f07da
Anthony Grimes <i@raynes.me>
2015-03-18 12:25:09 -0700

    First commit

---
+++ gitgot.clj
@@ -0,0 +1,5 @@
+(defn foo [x]
+  (let [x (x + 2)
+        y 3]
+    (println x)
+    (println y)))
---
+++ gotgit.js
@@ -0,0 +1,7 @@
+function foo(x) {
+  let x += 2;
+  let y = 3;
+
+  console.log(x);
+  console.log(y);
+}
